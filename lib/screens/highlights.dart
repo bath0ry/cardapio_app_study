e4e030bd9e366ee1d0c28b6a51c52bb61dd3c389
bath0ry <plylegacy@gmail.com>
2023-03-08 21:24:10 -0300

    add custom scroll view to highlights page

--- lib/screens/highlights.dart
+++ lib/screens/highlights.dart
@@ -11,14 +11,18 @@ class HighLights extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: ListView.builder(itemBuilder: (context, index) {
-        return HighlightItem(
-            imageURI: items[index]['image'],
-            itemTitle: items[index]['name'],
-            itemPrice: items[index]['price'],
-            itemDescription: items[index]['description']);
-      }),
-    );
+        padding: const EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            const SliverToBoxAdapter(child: Text('Destaques')),
+            SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+              return HighlightItem(
+                  imageURI: items[index]['image'],
+                  itemTitle: items[index]['name'],
+                  itemPrice: items[index]['price'],
+                  itemDescription: items[index]['description']);
+            }))
+          ],
+        ));
   }
 }
