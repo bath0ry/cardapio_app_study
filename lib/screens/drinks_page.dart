74399179314bf12f27ec87954ad6a846c04f5088
bath0ry <plylegacy@gmail.com>
2023-03-09 21:10:11 -0300

    create custom scroll view to drinks and menu

--- lib/screens/drinks_page.dart
+++ lib/screens/drinks_page.dart
@@ -1,11 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/drink_item.dart';
 
 class DrinksPage extends StatelessWidget {
-  const DrinksPage({super.key});
+  DrinksPage({super.key});
+  final List drinksItem = drinks;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text(
+                'Bebidas',
+                style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverGrid(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 170,
+                crossAxisSpacing: 11,
+                mainAxisSpacing: 16,
+              ),
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return DrinkItem(
+                    imageURI: drinksItem[index]['image'],
+                    itemTitle: drinksItem[index]['name'],
+                    itemPrice: drinksItem[index]['price']);
+              }, childCount: drinksItem.length))
+        ],
+      ),
+    );
   }
 }
--- lib/screens/menu_page.dart
+++ lib/screens/menu_page.dart
@@ -1,11 +1,36 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
-
+  final List foodItems = comidas;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+              child: Padding(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Text(
+              'Menu',
+              style: TextStyle(fontFamily: 'Caveat', fontSize: 32),
+              textAlign: TextAlign.center,
+            ),
+          )),
+          SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+            return FoodItem(
+                itemTitle: foodItems[index]['name'],
+                itemPrice: foodItems[index]['price'],
+                imageURI: foodItems[index]['image']);
+          }, childCount: foodItems.length))
+        ],
+      ),
+    );
   }
 }
